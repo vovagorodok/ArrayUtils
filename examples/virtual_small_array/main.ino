a9b69976be429958d8f4c6b0245a6a58bedfd8c8
Włodzimierz Ciesielski <vovagorodok2@gmail.com>
2022-10-20 00:53:00 +0200

    Better virt arr example

--- examples/virtual_small_array/main.ino
+++ examples/virtual_small_array/main.ino
@@ -1,7 +1,8 @@
 #include <VirtualSmallArray.h>
 #include <Arduino.h>
 
-VirtualSmallArray<int, 3> smallArray{1, 2, 3};
+VirtualSmallArray<int, 3> smallArray1{1, 2, 3};
+VirtualSmallArray<int, 2> smallArray2{4, 5};
 
 void print(const AbstractArray<int>& array) {
     for (const auto& el : array)
@@ -10,7 +11,8 @@ void print(const AbstractArray<int>& array) {
 
 void setup() {
     Serial.begin(115200);
-    print(smallArray);
+    print(smallArray1);
+    print(smallArray2);
 }
 
 void loop() {
