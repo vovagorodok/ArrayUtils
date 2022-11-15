89cbd72cdd9d532a5478520b06041c0ab3782263
Włodzimierz Ciesielski <vovagorodok2@gmail.com>
2022-11-15 12:32:14 +0100

    Used auto

--- src/ArrayUtils.h
+++ src/ArrayUtils.h
@@ -3,7 +3,7 @@
 #include <utility>
 
 template<typename T, std::size_t N, typename Converter, typename... OtherT>
-constexpr std::array<T, N> makeArray(Converter&& converter, OtherT&&... args)
+constexpr auto makeArray(Converter&& converter, OtherT&&... args)
 {
     std::array<T, N> res{converter(args)...};
     return res; 
